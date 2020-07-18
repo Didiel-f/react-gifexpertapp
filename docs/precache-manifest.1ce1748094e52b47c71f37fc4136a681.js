73ff1d637871ea3095bbd841244e4b2e76672819
Didiel Figueroa <didiel.f@gmail.com>
2020-07-18 01:13:23 -0400

    docs agregado

---
+++ docs/precache-manifest.1ce1748094e52b47c71f37fc4136a681.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3aadb28c37af5c25590d344a9fba169",
+    "url": "/index.html"
+  },
+  {
+    "revision": "690a70b83d4564c93512",
+    "url": "/static/css/main.de2361b1.chunk.css"
+  },
+  {
+    "revision": "85a4a86f3fe06f17fcc7",
+    "url": "/static/js/2.8249eaeb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8249eaeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "690a70b83d4564c93512",
+    "url": "/static/js/main.d96234a0.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
